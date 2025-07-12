f626663e53c952e4364773acb0969e791800dce0
Abdulrahman <abdulrahman.a.m.a.k.160@gmail.com>
2025-07-12 18:50:26 +0300

    fix: resolved a deployment related bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-    tailwindcss()
-  ],
-  base: process.env.VITE_BASE_PATH || "/ai-translator-react",
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react(), svgr(), tailwindcss()],
+    base: env.VITE_BASE_PATH || "/",
+  };
+});
