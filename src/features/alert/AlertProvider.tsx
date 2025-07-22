bef52de6c9e8140b0f91eaf184aa826064727879
Abdulrahman <abdulrahman.a.m.a.k.160@gmail.com>
2025-07-22 14:31:00 +0300

    style: extend alert transition time

--- src/features/alert/AlertProvider.tsx
+++ src/features/alert/AlertProvider.tsx
@@ -73,7 +73,7 @@ const AlertProvider = ({ children }: { children: ReactNode }) => {
           ${alertType === "info" ? "bg-gray-100/20 text-gray-800 dark:text-gray-200" : alertType === "success" ? "bg-green-300/30 dark:bg-green-400/30 text-green-700 dark:text-green-200" : alertType === "warning" ? "bg-yellow-800/25 text-yellow-200" : "bg-red-200/25 text-red-600 dark:text-red-500"}
           backdrop-blur-xs border-none rounded-md font-medium text-lg w-auto left-1/2
           -translate-x-1/2 truncate p-1.5 text-shadow-md/22 shadow-md
-          transition-[opacity,_top] duration-200
+          transition-[opacity,_top] duration-350
           ${isVisible ? "opacity-100 top-4" : "opacity-0 -top-4"}`}
       >
         {currentText && currentText}
