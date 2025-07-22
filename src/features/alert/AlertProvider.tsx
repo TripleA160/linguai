0c2784f2c5135132f7fab0d85c725f2be2ab6821
Abdulrahman <abdulrahman.a.m.a.k.160@gmail.com>
2025-07-22 18:48:42 +0300

    fix: remove pointer events from alert when visible so it doesn't overlap with other events

--- src/features/alert/AlertProvider.tsx
+++ src/features/alert/AlertProvider.tsx
@@ -71,7 +71,8 @@ const AlertProvider = ({ children }: { children: ReactNode }) => {
           ${alertType === "info" ? "bg-gray-400/15 dark:bg-gray-700/15 text-gray-900 dark:text-gray-200" : alertType === "success" ? "bg-green-300/20 dark:bg-green-700/20 text-green-800 dark:text-green-200" : alertType === "warning" ? "bg-yellow-200/15 dark:bg-yellow-700/25 text-yellow-600 dark:text-yellow-200" : "bg-red-200/20 dark:bg-red-800/10 text-red-600 dark:text-red-200"}
           backdrop-blur-xs border-none rounded-md text-lg w-auto left-1/2 -translate-x-1/2
           truncate p-1.5 text-shadow-md/22 shadow-md transition-[opacity,_top]
-          duration-350 ${isVisible ? "opacity-100 top-12" : "opacity-0 top-3"}`}
+          duration-350
+          ${isVisible ? "opacity-100 top-12 pointer-events-auto" : "opacity-0 top-3 pointer-events-none"}`}
       >
         {currentText && currentText}
       </div>
